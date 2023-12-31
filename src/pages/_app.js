731a8c200092c16aa0d4a01dbe3ffafab1fde376
SIDDHARTH SUTARIYA <siddharthpsutariya@gmail.com>
2024-01-01 00:52:30 +0530

    port commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,8 @@ import axios from 'axios'
 config.autoAddCss = false
 
   //axios.defaults.baseURL = `http://localhost:8080`;
-axios.defaults.baseURL = "https://jade-clear-caridea.cyclic.app";
+//axios.defaults.baseURL = "https://jade-clear-caridea.cyclic.app";
+axios.defaults.baseURL = "https://node-pahervesh-api.onrender.com";
 
 export default function App({ Component, pageProps }) {
 
