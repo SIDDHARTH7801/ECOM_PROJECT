1766aca63f834795d252fbf4c3e4dbb59a45b7a3
SIDDHARTH SUTARIYA <siddharthpsutariya@gmail.com>
2023-12-31 21:38:00 +0530

    Api commit

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+ //export const api = " http://localhost:8080";
  export const api = https://jade-clear-caridea.cyclic.app;
 // export const api = "https://lazy-erin-caridea-veil.cyclic.app"
 // // export const api = `https://apni-dukan.onrender.com`
